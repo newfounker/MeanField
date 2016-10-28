4f0d8cd04896cf6fe93fed3ca7ac1b168dffd7c8
Angelo Valli <angelo.valli@gmail.com>
2016-10-28 10:13:53 +0200

    Fixed the definition of the phase phi in the Hamiltonian of the bdg_haldane driver, yielding the opposite sign of the Chern number in the normal state.
    In the superconducting state the calculation of the Chern number
    cannob be calculated like in the normal state.
    We still have to figure out how to do it properly.

--- BdG/bdg_haldane.f90
+++ BdG/bdg_haldane.f90
@@ -2,41 +2,34 @@ program bdg_haldane
   USE SCIFOR
   USE DMFT_TOOLS
   implicit none
-  integer,parameter                           :: Norb=1,Nspin=1,Nlat=2
-  integer                                     :: Nso,Nlso
-  integer                                     :: Nloop
-  integer                                     :: Lmats,Lreal
-  integer                                     :: Nsuccess
-  integer                                     :: Lk
-
+  integer,parameter                       :: Norb=1,Nspin=1,Nlat=2
+  integer                                 :: Nso,Nlso
+  integer                                 :: Nloop
+  integer                                 :: Lmats,Lreal
+  integer                                 :: Nsuccess
+  integer                                 :: Lk
   !variables for the model:
-  integer                                     :: Nk,Nkpath
-  real(8)                                     :: Uloc
-  real(8)                                     :: ts,tsp,phi,delta,Mh,wmixing,xmu,eps
-  real(8)                                     :: beta
-  real(8)                                     :: Wdis
-  real(8)                                     :: nsc
-  real(8)                                     :: deltasc
-  real(8)                                     :: wini,wfin
-  real(8)                                     :: bdg_error
-
-
-  complex(8),allocatable,dimension(:,:,:)     :: Hk
-  complex(8),allocatable,dimension(:,:)       :: halHloc
-  complex(8),allocatable,dimension(:,:,:,:,:) :: Hloc
-  real(8),allocatable,dimension(:)            :: Wtk
-  real(8)                                     :: a,a0,bklen
-
-  real(8),dimension(2)                        :: d1,d2,d3
-  real(8),dimension(2)                        :: a1,a2,a3
-  real(8),dimension(2)                        :: bk1,bk2,pointK,pointKp
-
-
-  real(8),dimension(:),allocatable            :: nii,pii ![Nlat]
-  logical                                     :: converged,bool
-  integer                                     :: i,is,iloop,nrandom,idum
-  real(8),dimension(:),allocatable            :: wm,wr
-  character(len=50)                           :: finput
+  integer                                 :: Nk,Nkpath
+  real(8)                                 :: Uloc
+  real(8)                                 :: ts,tsp,phi,delta,Mh,wmixing,xmu,eps
+  real(8)                                 :: beta
+  real(8)                                 :: Wdis
+  real(8)                                 :: nsc
+  real(8)                                 :: deltasc
+  real(8)                                 :: wini,wfin
+  real(8)                                 :: bdg_error
+  !
+  complex(8),allocatable,dimension(:,:,:) :: HkNambu
+  real(8)                                 :: a,a0,bklen,chern,kx,ky
+  real(8),allocatable,dimension(:,:)      :: kvec
+  !
+  real(8),dimension(2)                    :: d1,d2,d3
+  real(8),dimension(2)                    :: a1,a2,a3
+  real(8),dimension(2)                    :: bk1,bk2,pointK,pointKp
+  real(8),dimension(:),allocatable        :: nii,pii,pii_prev ![Nlat]
+  logical                                 :: converged,bool
+  integer                                 :: ik,i,is,iloop,ix,iy
+  character(len=50)                       :: finput
 
   ! READ INPUT FILES !
   call parse_cmd_variable(finput,"FINPUT",default="inputBDG.conf")
@@ -104,25 +97,29 @@ program bdg_haldane
   pointKp= [2*pi/3,-2*pi/3/sqrt(3d0)]
 
 
-  ! ALLOCATE ALL THE REQUIRED VARIABLES !
+  ! BUILD THE RECIPROCAL VECTOR GRID !
+  Lk=Nk*Nk
+  write(*,*)"# of k-points     :",Lk
+  allocate(kvec(2,Lk))
+  ik=0
+  do iy=1,Nk
+     ky = dble(iy-1)/Nk
+     do ix=1,Nk
+        ik=ik+1
+        kx=dble(ix-1)/Nk
+        kvec(:,ik) = kx*bk1 + ky*bk2
+     enddo
+  enddo
+
+
+  ! SOLVE BDG PROBLEM !
   allocate(nii(Nlso))
   allocate(pii(Nlso))
-
-  ! ALLOCATE MATSUBARA AND REAL FREQUENCIES !
-  allocate(wm(Lmats),wr(Lreal))
-  wr = linspace(wini,wfin,Lreal)
-  wm = pi/beta*(2*arange(1,Lmats)-1)
-
-
-
-  ! GET THE TIGHT BINDING HAMILTONIAN FOR THE SQUARE LATTICE 
-  call build_hk("Hkfile_Haldane_sc.bdg")
-
-
+  allocate(pii_prev(Nlso))
   nii=nsc
   pii=deltasc
-  call read_data("nVSisite.bdg",nii)
-  call read_data("phiVSisite.bdg",pii)
+  call read_data("nVSisite.restart",nii)
+  call read_data("phiVSisite.restart",pii)
   iloop=0;
   converged=.false.;
   !+-------------------------------------+!
@@ -130,40 +127,41 @@ program bdg_haldane
      iloop=iloop+1
      call start_loop(iloop,nloop,"BdG-loop")
 
-     call BdG_Solve(nii,pii,Hk)
+     call BdG_Solve(nii,pii)
+     if(iloop>1)pii=wmixing*pii + (1d0-wmixing)*pii_prev
+     pii_prev=pii
      converged = check_convergence_local(pii,bdg_error,nsuccess,nloop,file="error.err")
      call print_sc_out(converged)
      call end_loop()
   enddo
   !+-------------------------------------+!
 
+  allocate(Hknambu(2*Nlso,2*Nlso,Lk))
+  do ik=1,Lk
+     Hknambu(:,:,ik) =  hk_NambuHaldane_model(kvec(:,ik),2*Nlso)
+  enddo
+  call get_Chern_number(Hknambu,[Nk,Nk],2,Nk/pi2*Nk/pi2,Chern)
+
+
+  call Build_Eigenbands()
 
 contains
 
 
 
-  subroutine BdG_Solve(nii,pii,H)
+  subroutine BdG_Solve(nii,pii)
     real(8),dimension(Nlso),intent(inout)         :: nii
     real(8),dimension(Nlso),intent(inout)         :: pii
-    complex(8),dimension(Nlso,Nlso,Lk),intent(in) :: H
-    real(8),dimension(Nlso)                       :: Sigma_HFB
-    real(8),dimension(Nlso)                       :: Self_HFB
     real(8),dimension(2*Nlso,2*Nlso)              :: Hknambu
     real(8),dimension(2*Nlso)                     :: Eknambu
     real(8),dimension(2*Nlso)                     :: RhoDiag  !diagonal density matrix 
     real(8),dimension(2*Nlso,2*Nlso)              :: RhoNambu !Nambu density matrix
     real(8),dimension(Nlso,Lk)                    :: nk,phik
     integer                                       :: i,ik
-    
     !
-    Sigma_HFB(:) =  Uloc*(nii-0.5d0)
-    Self_HFB(:)  =  Uloc*pii(:)
     do ik=1,Lk
        !
-       Hknambu(1:Nlso,1:Nlso)               =  H(:,:,ik) + diag(Sigma_HFB)
-       Hknambu(1:Nlso,Nlso+1:2*Nlso)        =    + diag(Self_HFB)
-       Hknambu(Nlso+1:2*Nlso,1:Nlso)        =    + diag(Self_HFB)
-       Hknambu(Nlso+1:2*Nlso,Nlso+1:2*Nlso) = -conjg(H(:,:,ik) + diag(Sigma_HFB))
+       Hknambu = hk_NambuHaldane_model(kvec(:,ik),2*Nlso)
        !
        call eigh(HkNambu,EkNambu)
        !
@@ -210,52 +208,23 @@ contains
     hk = h0*pauli_0 + hx*pauli_x + hy*pauli_y + hz*pauli_z
   end function hk_haldane_model
 
-
-
-
-
-
-  !---------------------------------------------------------------------
-  !PURPOSE: Get Haldane Model Hamiltonian
-  !---------------------------------------------------------------------
-  subroutine build_hk(file)
-    character(len=*),optional             :: file
-    integer                               :: i,j,ik
-    integer                               :: ix,iy
-    real(8)                               :: kx,ky    
-    integer                               :: iorb,jorb
-    integer                               :: isporb,jsporb
-    integer                               :: ispin,jspin
-    real(8),dimension(:),allocatable      :: kxgrid,kygrid
-    real(8),dimension(2)                  :: kvec
-    real(8),dimension(:,:),allocatable    :: kpath
-    !
-    write(*,*)"Build H(k) for Haldane model (now also Nobel Laureate):"
-    Lk=Nk*Nk
-    write(*,*)"# of k-points     :",Lk
-    write(*,*)"# of SO-bands     :",Nlso
-    !
-    if(allocated(Hk))deallocate(Hk)
-    if(allocated(wtk))deallocate(wtk)
-    !
-    allocate(Hk(Nlso,Nlso,Lk))
-    allocate(Wtk(Lk))
-    print*,"Build Hk(Nlso,Nlso) for the Haldane model"
-    ik=0
-    do iy=1,Nk
-       ky = dble(iy-1)/Nk
-       do ix=1,Nk
-          ik=ik+1
-          kx=dble(ix-1)/Nk
-          kvec = kx*bk1 + ky*bk2
-          Hk(:,:,ik) = hk_haldane_model(kvec,Nlso)
-       enddo
-    enddo
-    Wtk = 1d0/Lk
-    !
-    !
-  end subroutine build_hk
-
+  function hk_NambuHaldane_model(kpoint,N) result(hk)
+    real(8),dimension(:)            :: kpoint
+    integer                         :: N
+    complex(8),dimension(N,N)       :: hk
+    complex(8),dimension(Nlso,Nlso) :: hk11,hk22
+    real(8),dimension(Nlso)         :: sigma,self
+    real(8)                         :: h0,hx,hy,hz
+    if(2*Nlso/=N)stop "hk_NambuHaldane_model ERROR: 2*Nlso != N"
+    hk11  = hk_haldane_model(kpoint,Nlso)
+    hk22  = conjg(hk_haldane_model(-kpoint,Nlso))
+    sigma =  Uloc*(nii(:)-0.5d0)
+    self  =  Uloc*pii(:)
+    Hk(1:Nlso,1:Nlso)               =  hk11  + diag(sigma)
+    Hk(1:Nlso,Nlso+1:2*Nlso)        =        + diag(self)
+    Hk(Nlso+1:2*Nlso,1:Nlso)        =        + diag(self)
+    Hk(Nlso+1:2*Nlso,Nlso+1:2*Nlso) = -hk22  - diag(sigma) !Hk22=H*(-k)
+  end function hk_NambuHaldane_model
 
 
 
@@ -277,8 +246,8 @@ contains
     KPath(2,:)=pointK
     Kpath(3,:)=pointKp
     KPath(4,:)=[0d0,0d0]
-    call TB_Solve_path(hk_haldane_model,Nlso,KPath,Nkpath,&
-         colors_name=[red1,blue1],&
+    call TB_Solve_path(hk_NambuHaldane_model,2*Nlso,KPath,Nkpath,&
+         colors_name=[red1,blue1,green1,orange1],&
          points_name=[character(len=10) :: "G","K","K`","G"],&
          file="Eigenbands.bdg")
   end subroutine build_EigenBands
@@ -288,13 +257,6 @@ contains
 
 
 
-
-
-
-
-
-
-
   ! !******************************************************************
   ! !******************************************************************
 
@@ -302,68 +264,113 @@ contains
 
 
 
-
-  subroutine ChernNumber
-    complex(8),dimension(:,:,:),allocatable :: BlochStates
-    real(8),dimension(:,:),allocatable      :: Berry_curvature
-    integer                                 :: ik,ix,iy
-    complex(8)                              :: Eigvec(Nlso,Nlso)
-    real(8)                                 :: Chern,BZ_area,eigval(Nlso)
-    real(8),dimension(:),allocatable        :: kxgrid
-    !CHERN NUMBERS:
-    allocate(BlochStates(Nlso,Nk,Nk))
-    allocate(Berry_curvature(Nk,Nk))
-    chern=0d0
-    ik=0
-    do ix=1,Nk
-       do iy=1,Nk
-          ik=ik+1
-          Eigvec = Hk(:,:,ik) !-  nnn2lso_reshape(S0,Nlat,Nspin,Norb)
-          call eigh(Eigvec,Eigval)
-          BlochStates(:,ix,iy) = Eigvec(:,1)
-       enddo
-    enddo
-    call get_Chern_number(BlochStates,Chern,Berry_curvature,Nk/pi2*Nk/pi2)
-    write(*,*)"Chern =",chern
-    allocate(kxgrid(Nk))
-    kxgrid=linspace(0d0,pi2,Nk)
-    call splot3d("Berry_Curvature.nint",kxgrid,kxgrid,Berry_Curvature)
-    open(10,file="ChernNumber.dat")
-    write(10,*)int(chern)
-    close(10)
-  end subroutine ChernNumber
-  !
-  subroutine Get_Chern_number(State,Chern_number,Berry_curvatures,one_over_area)
-    complex(8),intent(in),dimension(:,:,:)                     :: state !(nhilb, nk1, nk2)
-    real(8),intent(out)                                        :: chern_number
-    real(8),intent(out),dimension(size(state,2),size(state,3)) :: berry_curvatures
-    real(8),intent(in)                                         :: one_over_area
-    integer                                                    :: nhilb,nk1,nk2
-    integer                                                    :: i1,i2,i3,ix,i1p,i1m,i2m,i2p,it
-    complex(8)                                                 :: path_scalar_products(4)
-    real(8)                                                    :: berry_phase
+  ! calcola il numero di chern di un generico stato dipendente da k con il metodo di Resta
+  subroutine Get_Chern_number(Hk,Nkvec,Noccupied,one_over_area,Chern)
+    complex(8),intent(in),dimension(:,:,:)    :: Hk    ![Nlso][Nlso][Nktot]
+    integer,intent(in),dimension(2)           :: Nkvec ![Nk1][Nk2]: prod(Nkvec)=Nktot
+    integer,intent(in)                        :: Noccupied
+    real(8),intent(in)                        :: one_over_area
+    real(8),intent(out)                       :: Chern
     !
-    Nhilb= size(state,1)
-    Nk1  = size(state,2)
-    Nk2  = size(state,3)
-    chern_number = zero
-    do i1= 1, nk1
-       i1p = modulo(i1,nk1) + 1
-       i1m = modulo(i1-2,nk1) + 1
-       do i2= 1, nk2           !faccio l'integrale sulla bz
-          i2p = modulo(i2,nk2) + 1
-          i2m = modulo(i2-2,nk2) + 1
-          path_scalar_products(1) = dot_product(state(:,i1,i2),state(:,i1, i2p))
-          path_scalar_products(2) = dot_product(state(:,i1,i2p),state(:,i1p, i2p))
-          path_scalar_products(3) = dot_product(state(:,i1p,i2p),state(:,i1p, i2))
-          path_scalar_products(4) = dot_product(state(:,i1p,i2),state(:,i1,i2))
-          berry_phase = -dimag(zlog( product(path_scalar_products)  ))
-          berry_curvatures(i1,i2) = berry_phase*one_over_area
-          chern_number = chern_number + berry_phase
+    integer                                   :: Nlso
+    integer                                   :: Nktot
+    integer                                   :: Nkx,Nky
+    integer                                   :: ikx,iky
+    integer                                   :: ikxP,ikyP
+    integer                                   :: ik,iocc,i,j
+    complex(8),dimension(:,:),allocatable     :: Eigvec ![Nlso][Nlso]
+    real(8),dimension(:),allocatable          :: Eigval ![Nlso]
+    complex(8),dimension(:,:),allocatable     :: Gmat
+    complex(8),dimension(:,:,:,:),allocatable :: BlochStates ![Nkx][Nky][Noccupied][Nlso]
+    complex(8),dimension(4)                   :: Ulink
+    real(8),dimension(:,:),allocatable        :: BerryCurvature
+    real(8)                                   :: berry_phase
+    integer                                   :: unit
+    !
+    Nlso  = size(Hk,1)
+    Nktot = size(Hk,3)
+    Nkx   = Nkvec(1)
+    Nky   = Nkvec(2)
+    call assert_shape(Hk,[Nlso,Nlso,Nktot],"Get_Chern_NUmber_NEW","Hk")
+    if(Nkx*Nky/=Nktot)stop "ERROR Get_Chern_Number_NEW: Nktot = prod(Nkvec)"
+    !
+    !
+    !1. Get the Bloch states from H(:,:,k)
+    allocate(Eigvec(Nlso,Nlso))
+    allocate(Eigval(Nlso))
+    allocate(BlochStates(Nkx,Nky,Noccupied,Nlso))
+    allocate(BerryCurvature(Nkx,Nky))
+    allocate(Gmat(Noccupied,Noccupied))
+    ik=0
+    do ikx=1,Nkx
+       do iky=1,Nky
+          ik=ik+1
+          Eigvec = Hk(:,:,ik)
+          call eigh(Eigvec,Eigval)
+          do iocc=1,Noccupied
+             BlochStates(ikx,iky,iocc,:) = Eigvec(:,iocc)
+          enddo
        enddo
     enddo
-    chern_number = chern_number/pi2
+    deallocate(Eigvec,Eigval)
+    !
+    !
+    !2. Evaluate the Berry Curvature
+    chern=0d0
+    do ikx= 1, Nkx
+       ikxP = modulo(ikx,Nkx) + 1
+       !ikxM = modulo(ikx-2,Nkx) + 1
+       do iky= 1, Nky
+          ikyP = modulo(iky,Nky) + 1
+          !ikyM = modulo(iky-2,Nky) + 1
+          !
+          if(Noccupied==1)then
+             Ulink(1) = dot_product(BlochStates(ikx,iky,1,:)  , BlochStates(ikx,ikyP,1,:))
+             Ulink(2) = dot_product(BlochStates(ikx,ikyP,1,:) , BlochStates(ikxP,ikyP,1,:))
+             Ulink(3) = dot_product(BlochStates(ikxP,ikyP,1,:), BlochStates(ikxP,iky,1,:))
+             Ulink(4) = dot_product(BlochStates(ikxP,iky,1,:) , BlochStates(ikx,iky,1,:))
+             !
+          else
+             !
+             forall(i=1:Noccupied,j=1:Noccupied)&
+                  gmat(i,j)=dot_product(BlochStates(ikx,iky,i,:)  , BlochStates(ikx,ikyP,j,:))
+             Ulink(1) = det(gmat)
+             !
+             forall(i=1:Noccupied,j=1:Noccupied)&
+                  gmat(i,j) = dot_product(BlochStates(ikx,ikyP,i,:) , BlochStates(ikxP,ikyP,j,:))
+             Ulink(2) = det(gmat)
+             !
+             forall(i=1:Noccupied,j=1:Noccupied)&
+                  gmat(i,j) = dot_product(BlochStates(ikxP,ikyP,i,:), BlochStates(ikxP,iky,j,:))
+             Ulink(3) = det(gmat)
+             !
+             forall(i=1:Noccupied,j=1:Noccupied)&
+                  gmat(i,j) = dot_product(BlochStates(ikxP,iky,i,:) , BlochStates(ikx,iky,j,:))
+             Ulink(4) = det(gmat)
+             !
+          endif
+          !
+          berry_phase = dimag(zlog( product(Ulink(:))  ))
+          chern = chern + berry_phase
+          BerryCurvature(ikx,iky) = berry_phase*one_over_area
+          !
+       enddo
+    enddo
+    !
+    chern=chern/pi2
+    !
+    open(unit=free_unit(unit),file="Chern_Number.dat")
+    write(unit,*)chern
+    write(*,*)chern
+    close(unit)
+    !
+    call splot3d("Berry_Curvature.dat",&
+         linspace(0d0,pi2,Nkx,iend=.false.),&
+         linspace(0d0,pi2,Nky,iend=.false.),&
+         BerryCurvature(:Nkx,:Nky))
+    !
   end subroutine Get_Chern_number
+
 
 
 
@@ -376,41 +383,33 @@ contains
     complex(8),dimension(2,Nlso,Nlso,Lmats) :: Gmats,fooSmats
     complex(8),dimension(2,Nlso,Nlso,Lreal) :: Greal,fooSreal
     !
-
-
     print*,"<nimp>  =",2*nii
     print*,"<phi>   =",pii
-
+    !
     call splot("nVSiloop.bdg",iloop,2*nii(1),2*nii(2),append=.true.)
     call splot("phiVSiloop.bdg",iloop,pii(1),pii(2),append=.true.)
-
-
+    !
     !Save the guess:
     call store_data("nVSisite.bdg",nii(:))
     call store_data("phiVSisite.bdg",pii(:))
-
     if(converged)then
        !Build the local GF:
-
        open(10,file="density.bdg")
        write(10,"(10F20.12)")(2*nii(iorb),iorb=1,Nlso)
        close(10)
        open(10,file="phi.bdg")
        write(10,"(10F20.12)")(pii(iorb),iorb=1,Nlso)
        close(10)
-
-       do i=1,Lmats
-          fooSmats(1,:,:,i)=diag(Uloc*(nii-0.5d0))
-          fooSmats(2,:,:,i)=diag(Uloc*pii(:))
-       enddo
-       call dmft_gloc_matsubara_superc(Hk,Wtk,Gmats,fooSmats,iprint=4)
-
-       do i=1,Lreal
-          fooSreal(1,:,:,i)=diag(Uloc*(nii-0.5d0))
-          fooSreal(2,:,:,i)=diag(Uloc*pii(:))
-       enddo
-       call dmft_gloc_realaxis_superc(Hk,Wtk,Greal,fooSreal,iprint=4)
-
+       ! do i=1,Lmats
+       !    fooSmats(1,:,:,i)=diag(Uloc*(nii-0.5d0))
+       !    fooSmats(2,:,:,i)=diag(Uloc*pii(:))
+       ! enddo
+       ! call dmft_gloc_matsubara_superc(Hk,Wtk,Gmats,fooSmats,iprint=4)
+       ! do i=1,Lreal
+       !    fooSreal(1,:,:,i)=diag(Uloc*(nii-0.5d0))
+       !    fooSreal(2,:,:,i)=diag(Uloc*pii(:))
+       ! enddo
+       ! call dmft_gloc_realaxis_superc(Hk,Wtk,Greal,fooSreal,iprint=4)
        ! do iorb=1,Nlso
        !    n0(iorb) = fft_get_density(Gmats(1,iorb,iorb,:),beta)
        !    phi0(iorb) = fft_get_density(Gmats(2,iorb,iorb,:),beta,notail=.true.)
@@ -422,8 +421,12 @@ contains
        ! write(10,"(10F20.12)")(phi0(iorb),iorb=1,Nlso)
        ! close(10)
     endif
-
   end subroutine print_sc_out
+
+
+
+
+
 
 
 
